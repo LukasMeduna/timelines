8bcfdced9d49848ec87830d4a475882ccae49ee9
LukasMeduna <lukas.meduna@gmail.com>
2022-03-31 14:28:59 +0200

    changed timeUnits to objects with properties: id and name

--- src/components/timeUnitsRow/timeUnitsRow.jsx
+++ src/components/timeUnitsRow/timeUnitsRow.jsx
@@ -3,7 +3,7 @@ import "./timeUnitsRow.css";
 export default function TimeUnitsRow(props) {
     return (
         <div className="timeUnitsRow">
-            {props.timeUnits.map((timeUnit) => <div className="timeUnit" key={timeUnit} style={{width: props.timeUnitWidth+"px"}}>{timeUnit}</div>)}
+            {props.timeUnits.map((timeUnit) => <div className="timeUnit" key={timeUnit.id} style={{width: props.timeUnitWidth+"px"}}>{timeUnit.name}</div>)}
         </div>
     )
 }
--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,9 +1,9 @@
 import "./timeline.css";
 
 function TimelineBox(props) {
-    const startUnitNumber = props.timeUnits.indexOf(props.box.startingTimeUnit);
+    const startUnitNumber = props.timeUnits.map(x => x.id).indexOf(props.box.startingTimeUnit);
     const startPosition = startUnitNumber*props.timeUnitWidth+Math.round(props.box.startingPosition*props.timeUnitWidth);
-    const endUnitNumber = props.timeUnits.indexOf(props.box.endingTimeUnit);
+    const endUnitNumber = props.timeUnits.map(x => x.id).indexOf(props.box.endingTimeUnit);
     const endPosition = endUnitNumber*props.timeUnitWidth+Math.round(props.box.endingPosition*props.timeUnitWidth);
     const boxWidth = endPosition-startPosition;
 
@@ -18,8 +18,11 @@ function TimelineBox(props) {
 
 export default function Timeline(props) {
     const totalTimeUnits = props.timeUnits.length;
+
+    const addTimelineBox = () => { alert('Ahoj')}
+
     return (
-        <div className="timeline" style={{height: props.timeline.rows*40+"px", width: totalTimeUnits*props.timeUnitWidth+"px"}}>
+        <div className="timeline" style={{height: props.timeline.rows*40+"px", width: totalTimeUnits*props.timeUnitWidth+"px"}} onClick={addTimelineBox}>
             {props.timeline.timelineBoxes.map(box => <TimelineBox box={box} key={box.id} timeUnitWidth={props.timeUnitWidth} timeUnits={props.timeUnits} />)}
         </div>
     )
