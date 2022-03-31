be0b03b6419cc7b5baa245c1fc6fb1d50c4d7edc
LukasMeduna <lukas.meduna@gmail.com>
2022-03-31 11:10:18 +0200

    Deleted unused lines

--- src/App.js
+++ src/App.js
@@ -10,18 +10,10 @@ function App() {
   const [data, setData] = useState();
   const uploadData = (uploadedData) => setData(uploadedData);
 
-  /*const [unitWidth, setUnitWidth] = useState(data.timeUnitWidth);
-  const zoomIn = () => setUnitWidth(unitWidth+10);
-  const zoomOut = () => setUnitWidth(unitWidth-10);*/
-
   const zoomIn = () => { setData(previousState => { return { ...previousState, timeUnitWidth: data.timeUnitWidth + 10 } }); };
   const zoomOut = () => { setData(previousState => { return { ...previousState, timeUnitWidth: data.timeUnitWidth - 10 } }); };
 
-
-
   if (data) {
-
-
     return (
       <div className="App">
         <Topbar zoomIn={zoomIn} zoomOut={zoomOut} />
@@ -30,7 +22,6 @@ function App() {
           {data.myTimelines.map(myTimeline => <TimelineName key={myTimeline.id} timeline={myTimeline} />)}
         </div>
         <div className="timelinesArea">
-
           <TimeUnitsRow timeUnits={data.timeUnits} timeUnitWidth={data.timeUnitWidth} />
           {data.myTimelines.map(myTimeline => <Timeline key={myTimeline.id} timeUnits={data.timeUnits} timeUnitWidth={data.timeUnitWidth} timeline={myTimeline} />)}
         </div>
