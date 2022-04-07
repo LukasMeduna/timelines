da5e3049160b8a5486018b2f1a35ca024dfd2bb4
LukasMeduna <lukas.meduna@gmail.com>
2022-04-07 12:38:51 +0200

    component for TimeUnit

--- src/components/timeUnitsRow/timeUnitsRow.jsx
+++ src/components/timeUnitsRow/timeUnitsRow.jsx
@@ -1,9 +1,17 @@
 import "./timeUnitsRow.css";
 
+function TimeUnit(props) {
+    return (
+        <div className="timeUnit" style={{width: props.timeUnitWidth+"px"}}>
+            {props.timeUnitName}
+        </div>
+    )
+}
+
 export default function TimeUnitsRow(props) {
     return (
         <div className="timeUnitsRow">
-            {props.timeUnits.map((timeUnit) => <div className="timeUnit" key={timeUnit.id} style={{width: props.timeUnitWidth+"px"}}>{timeUnit.name}</div>)}
+            {props.timeUnits.map((timeUnit) => <TimeUnit key={timeUnit.id} timeUnitWidth={props.timeUnitWidth} timeUnitName = {timeUnit.name} />)}
         </div>
     )
 }
