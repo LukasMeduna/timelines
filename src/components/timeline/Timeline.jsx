8e332acac37892269897f3e7d75dd262cfaf17c3
LukasMeduna <lukas.meduna@gmail.com>
2022-04-07 09:33:55 +0200

    TimelineBox moved to a separate file

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,76 +1,5 @@
 import "./timeline.css";
-
-function TimelineBox(props) {
-    const startUnitNumber = props.timeUnits.map(x => x.id).indexOf(props.box.startingTimeUnit);
-    const startPosition = startUnitNumber*props.timeUnitWidth+Math.round(props.box.startingPosition*props.timeUnitWidth);
-    const endUnitNumber = props.timeUnits.map(x => x.id).indexOf(props.box.endingTimeUnit);
-    const endPosition = endUnitNumber*props.timeUnitWidth+Math.round(props.box.endingPosition*props.timeUnitWidth);
-    const boxWidth = endPosition-startPosition;
-    const htmlId = props.timelineId+"box"+props.id;
-
-    function initLeftResize() {
-        window.addEventListener('mousemove', leftResize);
-        window.addEventListener('mouseup', stopLeftResize);
-    }
-    function leftResize(e) {
-        const rect = document.querySelector(".timeUnitsRow").getBoundingClientRect();
-        const timelineHorizontalPosition = rect.left;
-        const newStartPosition = e.pageX-timelineHorizontalPosition;
-        const newBoxWidth = endPosition - e.pageX+timelineHorizontalPosition;
-        if (newBoxWidth>=0 && (e.pageX-rect.left)>0 ) {
-            document.getElementById(htmlId).style.left = newStartPosition + 'px';
-            document.getElementById(htmlId).style.width = newBoxWidth + 'px';
-        }
-    }
-    function stopLeftResize() {
-        window.removeEventListener('mousemove', leftResize);
-        window.removeEventListener('mouseup', stopLeftResize);
-        updateBox();
-    }
-
-    function initRightResize() {
-        window.addEventListener('mousemove', rightResize);
-        window.addEventListener('mouseup', stopRightResize);
-    }
-    function rightResize(e) {
-        const rect = document.querySelector(".timeline").getBoundingClientRect();
-        const timelineHorizontalPosition = rect.left;
-        const newBoxWidth = e.pageX - timelineHorizontalPosition - startPosition + 2;
-        if (newBoxWidth>=2 && (rect.right-e.screenX)>=0) {
-            document.getElementById(htmlId).style.width = newBoxWidth + 'px';
-        }
-    }
-    function stopRightResize() {
-        window.removeEventListener('mousemove', rightResize);
-        window.removeEventListener('mouseup', stopRightResize);
-        updateBox();
-    }
-
-    function updateBox() {
-        const leftPosition = parseInt(document.getElementById(htmlId).style.left);
-        const width = parseInt(document.getElementById(htmlId).style.width);
-        const rightPosition = leftPosition + width;
-        
-        let newBox = props.box;
-        newBox.startingTimeUnit = props.timeUnits[Math.floor(leftPosition / props.timeUnitWidth)].id;
-        newBox.startingPosition = (leftPosition % props.timeUnitWidth) / props.timeUnitWidth;
-        newBox.endingPosition = (rightPosition % props.timeUnitWidth) / props.timeUnitWidth;
-        try {newBox.endingTimeUnit = props.timeUnits[Math.floor(rightPosition / props.timeUnitWidth)].id;}
-        catch(err) {
-            newBox.endingTimeUnit = props.timeUnits[props.timeUnits.length-1].id;
-            newBox.endingPosition = 1}
-        props.updateTimelineBox(props.timelineId,newBox);
-    }
-
-    return (
-        <div onMouseDown={(e) => {e.stopPropagation()}} className="timelineBoxContainer" id={props.timelineId+"box"+props.id} style={{top: props.box.row*40+"px", left: startPosition+"px", width: boxWidth+"px"}}>
-            <div onMouseDown={initLeftResize} className="resizerLeft"></div>
-            <div className="timelineBox" style={{backgroundColor: props.box.bgColor}} title={props.box.text}>{props.box.text}</div>
-            <div onMouseDown={initRightResize} className="resizerRight"></div>
-        </div>
-    );
-}
-
+import { TimelineBox } from "../timelineBox/TimelineBox";
 
 export default function Timeline(props) {
     const totalTimeUnits = props.timeUnits.length;
@@ -80,12 +9,6 @@ export default function Timeline(props) {
         if (!boxText) return;
         const coordinateX = e.nativeEvent.offsetX;
         const coordinateY = e.nativeEvent.offsetY;
-        /*if (e.type === "mousedown") {
-            value = e.nativeEvent.offsetX;
-        } else {
-            value -= e.nativeEvent.offsetX;
-            console.log("value:",value);
-        }*/
         const startingTimeUnit = props.timeUnits[Math.floor(coordinateX / props.timeUnitWidth)].id;
         const startingPosition = (coordinateX % props.timeUnitWidth) / props.timeUnitWidth;
         let endingTimeUnit;
